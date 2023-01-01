2691c0a6259ded322282d9db9688bbb23310d097
corey <cc.seer@gmail.com>
2023-01-01 16:03:25 -0500

    add print

--- seer/setup4seer.py
+++ seer/setup4seer.py
@@ -5,15 +5,17 @@ import sys
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 path_pip_py = os.path.join(cur_dir, "get-pip.py")
-# print(path_pip_py, sys.executable)
-ret = subprocess.run([sys.executable, path_pip_py])
+cmd = [sys.executable, path_pip_py]
+print(cmd)
+ret = subprocess.run(cmd)
 if ret.returncode != 0:
     exit(ret.returncode)
 
 path_pip = os.path.realpath(os.path.join(cur_dir, "../Scripts/pip.exe"))
 if os.path.exists(path_pip) is False:
     exit(-1)
-ret = subprocess.run(
-    [path_pip, "install", "-r", os.path.join(cur_dir, "requirements.txt")]
-)
+
+cmd = [path_pip, "install", "-r", os.path.join(cur_dir, "requirements.txt")]
+print(cmd)
+ret = subprocess.run(cmd)
 exit(ret.returncode)
