1a86889754285bfa31d4332af33af9353fa95bf4
corey <cc.seer@gmail.com>
2023-01-05 21:29:16 -0500

    add -w

--- controls/zipfolder/zipfolder.py
+++ controls/zipfolder/zipfolder.py
@@ -2,7 +2,7 @@ SCRIPT_INFO = {
     "name": "zipfolder",
     "type": 1,
     "extensions": ["${type_folder}"],
-    "arguments": ["-e", "${7z}", "-i", "${input_file}"],
+    "arguments": ["-e", "${7z}", "-i", "${input_file}", "-w"],
     "author": "Corey",
     "version": "1.0.0",
     "description": "Compress the folder to a zip file",
@@ -15,6 +15,9 @@ def parse_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", help="7z.exe file path", required=True)
     parser.add_argument("-i", help="input file path", required=True)
+    parser.add_argument(
+        "-w", help="whether open output window", required=False, action="store_true"
+    )
     args = parser.parse_args()
     return vars(args)
 
@@ -26,12 +29,12 @@ if __name__ == "__main__":
 
     args = parse_arg()
 
-    path_7z = args["e"]
     input_path = args["i"]
-
     filename = os.path.splitext(os.path.basename(input_path))[0]
     # overwrite when exists
     output_file = os.path.realpath(input_path + ".zip")
 
-    ret = subprocess.run([path_7z, "a", output_file, input_path])
+    ret = subprocess.run([args["e"], "a", output_file, input_path])
+    if ret.returncode == 0 and args["w"] is True:
+        subprocess.Popen(["explorer", "/select,", os.path.normpath(output_file)])
     sys.exit(ret.returncode)
